a890e45610dac526e79f5b98e67c4d4640768d91
Antonio Thomacelli <tonnytg@gmail.com>
2022-02-21 23:42:18 -0300

    feat: test localtion

--- entity/location/location.go
+++ entity/location/location.go
@@ -1,17 +1,39 @@
 package location
 
+import "fmt"
+
+var (
+	ErrCepSyntax   = fmt.Errorf("CEP syntax error")
+	ErrCepNotFound = fmt.Errorf("CEP not found")
+	ErrCepEmpty    = fmt.Errorf("CEP empty")
+)
+
 type Location struct {
-	CEP string `json:"cep"`
-	Street string `json:"street"`
-	Number []int `json:"number"`
-	City string `json:"city"`
-	State string `json:"state"`
-	Country string `json:"country"`
+	CEP       string `json:"cep"`
+	CepStatus bool   `json:"cep_status"`
+	StatusMsg string `json:"status_msg"`
+	Street    string `json:"street"`
+	Number    []int  `json:"number"`
+	City      string `json:"city"`
+	State     string `json:"state"`
+	Country   string `json:"country"`
 }
 
-func (l *Location) SetCEP(c string) error {
+func (l *Location) CheckCEP() {
+
+	l.CepStatus = true
+	l.StatusMsg = "status: ok"
+
+	if len(l.CEP) != 8 {
+		l.CepStatus = false
+		l.StatusMsg = "must be 8 characters"
+	}
+}
+
+func (l *Location) SetCEP(c string) {
+
 	l.CEP = c
-	return nil
+	l.CheckCEP()
 }
 
 func (l *Location) GetCEP() string {
@@ -19,5 +41,7 @@ func (l *Location) GetCEP() string {
 }
 
 func NewLocation() Location {
-	return Location{}
+	l := Location{}
+	l.CepStatus = false
+	return l
 }
--- entity/location/location_test.go
+++ entity/location/location_test.go
@@ -6,8 +6,12 @@ import (
 
 func TestSetCEP(t *testing.T) {
 	location := NewLocation()
-	location.SetCEP("12345-678")
-	if location.CEP != "12345-678" {
+	location.SetCEP("12345678")
+	if location.CEP != "12345678" {
 		t.Errorf("CEP was not set correctly")
+	}
+
+	if location.Street != "" {
+		t.Errorf("Street was not set correctly")
 	}
 }
